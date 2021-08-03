7fbe4d664876ce7e6c89d21660b48bd630df55e5
Tatiana Johnson <Tatianarjo@gmail.com>
2021-08-02 22:15:25 -0500

    first commit

---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels Noth</h3>
+        <div className="location__street">200 Main Street</div>
+    </section>
+)
