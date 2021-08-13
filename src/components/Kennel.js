dcc533dd806ea88ee005f774a10aabaf230a686b
Tatiana Johnson <Tatianarjo@gmail.com>
2021-08-12 20:44:22 -0500

    added registration and login

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,23 +1,33 @@
-import React from "react"
+import React from "react";
 // import { Animal } from "./animal/Animal"
-import "./Kennel.css"
-import { Employee } from "./employee/Employee"
-import { EmployeeList } from "./employee/EmployeeList"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-// import { Location } from "./location/Location"
-import { LocationList } from "./location/LocationList"
-import { LocationProvider } from "./location/LocationProvider"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import { AnimalList } from "./animal/AnimalList"
-import { CustomerProvider } from "./customer/CustomerProvider"
-import { CustomerList } from "./customer/CustomerList"
-import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationViews"
+import "./Kennel.css";
+import { Route, Redirect } from "react-router-dom";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
 
 export const Kennel = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-
+    <> 
+    <Route
+        render={() => {
+            if (localStorage.getItem("kennel_customer")) {
+                return (
+                    <>
+                     <NavBar />
+                        <ApplicationViews />
+                    </>
+                );
+            } else {
+                return <Redirect to="/login" />;
+            }
+        }}
+        />
+        <Route path="/login">
+            <Login />
+        </Route>
+        <Route path="/register">
+            <Register />
+        </Route>
     </>
-)
+);
